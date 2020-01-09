f3fae426fa21aa0d20a2c5b5156c408b49945eee
koda <hogehogehoge@gmail.com>
2020-01-10 00:49:26 +0900

    controllrの記載

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,22 +1,49 @@
 class QuestionsController < ApplicationController
+  before_action :set_question, only:[:show, :edit, :update, :destroy]
   def index
+    @questions = Question.all
   end
 
   def new
+    @question = Question.new
   end
 
   def create
+    @question = Question.create(question_params)
+    if @question.save
+      redirect_to questions_path
+    else
+      render :new
+    end
   end
 
-  def update
-  end
-
-  def destroy
+  def show
   end
 
   def edit
   end
 
-  def show
+  def update
+    if @question.update(question_params)
+      redirect_to questions_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @question.destroy
+    redirect_to questions_path
+  end
+
+
+  private
+
+  def question_params
+    params.require(:question).permit(:user_name, :title, :content)
+  end
+
+  def set_question
+    @question = Question.find(params[:id])
   end
 end
