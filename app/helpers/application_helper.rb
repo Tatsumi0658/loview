0fcac19ec8ca9b5904c4438100510ffdf54111f1
koda <hogehogehoge@gmail.com>
2020-01-22 00:29:22 +0900

    currentuser設定の変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
   def current_account
-    @current_account ||= Account.find_by(user_id: current_user.id)
+    if current_user.nil? || Account.find_by(user_id: current_user.id).nil?
+      @current_account = nil
+    else
+      @current_account ||= Account.find_by(user_id: current_user.id)
+    end
   end
 end
