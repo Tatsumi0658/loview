86dbf73649b76a28aad9e8003a1b169991db4b9e
koda <hogehogehoge@gmail.com>
2020-01-21 20:28:24 +0900

    user_idでのログイン機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,21 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :rememberable, :recoverable, :validatable, authentication_keys: [:user_id]
+
+  validates :user_id, presence: true, uniqueness: { case_sensitive: :true }
+
+
+  def email_required?
+    false
+  end
+
+  def email_changed?
+    false
+  end
+
+  def will_save_change_to_email?
+    false
+  end
+
 end
